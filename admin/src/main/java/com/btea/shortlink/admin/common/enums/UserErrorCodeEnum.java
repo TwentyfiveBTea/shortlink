84beeb2bf1c76b21781379e3d69f43f39fda2b38
BTea <1248102041@qq.com>
2025-02-05 13:48:27 +0800

    开发用户自定义异常码

---
+++ admin/src/main/java/com/btea/shortlink/admin/common/enums/UserErrorCodeEnum.java
@@ -0,0 +1,34 @@
+package com.btea.shortlink.admin.common.enums;
+
+import com.btea.shortlink.admin.common.convention.errorcode.IErrorCode;
+
+/**
+ * @Author: TwentyFiveBTea
+ * @Date: 2025/02/04 18:20
+ * @Description: 用户错误码
+ */
+public enum UserErrorCodeEnum implements IErrorCode {
+    USER_NULL("B000200", "用户记录不存在"),
+    USER_NAME_EXIST("B000201", "用户名已存在"),
+    USER_EXIST("B000202", "用户记录已存在"),
+    USER_SAVE_ERROR("B000203", "用户记录新增失败");
+
+    private final String code;
+
+    private final String message;
+
+    UserErrorCodeEnum(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    @Override
+    public String code() {
+        return code;
+    }
+
+    @Override
+    public String message() {
+        return message;
+    }
+}
