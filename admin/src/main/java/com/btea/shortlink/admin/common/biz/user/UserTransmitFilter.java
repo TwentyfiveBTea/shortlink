0c15c854ff0ec3b36f8b21edbf57d24a5ebb4373
TwentyfiveBTea <1248102041@qq.com>
2025-02-25 21:23:20 +0800

    开发用户登录验证拦截器返回友好提示信息

--- admin/src/main/java/com/btea/shortlink/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/com/btea/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -1,15 +1,23 @@
 package com.btea.shortlink.admin.common.biz.user;
 
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson2.JSON;
+import com.btea.shortlink.admin.common.convention.exception.ClientException;
+import com.btea.shortlink.admin.common.convention.result.Results;
 import com.google.common.collect.Lists;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 import java.util.Objects;
+
+import static com.btea.shortlink.admin.common.enums.UserErrorCodeEnum.USER_TOKEN_FAIL;
 
 /**
  * @Author: TwentyFiveBTea
@@ -26,15 +34,30 @@ public class UserTransmitFilter implements Filter {
             "api/short-link/admin/v1/user/has-username"
     );
 
+    @SneakyThrows
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         String requestURI = httpServletRequest.getRequestURI();
-        if (!Objects.equals(requestURI, "/api/short-link/admin/v1/user/login")) {
-            String username = httpServletRequest.getHeader("username");
-            String token = httpServletRequest.getHeader("token");
-            Object userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
-            if (userInfoJsonStr != null) {
+        if (!IGNOGE_URI.contains(requestURI)) {
+            String method = httpServletRequest.getMethod();
+            if (!(Objects.equals(requestURI, "/api/short-link/admin/v1/user") && Objects.equals(method, "POST"))) {
+                String username = httpServletRequest.getHeader("username");
+                String token = httpServletRequest.getHeader("token");
+                if (!StrUtil.isAllBlank(username, token)) {
+                    returnJson((HttpServletResponse) servletResponse, JSON.toJSONString(Results.failure(new ClientException(USER_TOKEN_FAIL))));
+                    return;
+                }
+                Object userInfoJsonStr;
+                try {
+                    userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
+                    if (userInfoJsonStr == null) {
+                        throw new ClientException(USER_TOKEN_FAIL);
+                    }
+                } catch (Exception ex) {
+                    returnJson((HttpServletResponse) servletResponse, JSON.toJSONString(Results.failure(new ClientException(USER_TOKEN_FAIL))));
+                    return;
+                }
                 UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
                 UserContext.setUser(userInfoDTO);
             }
@@ -43,6 +66,21 @@ public class UserTransmitFilter implements Filter {
             filterChain.doFilter(servletRequest, servletResponse);
         } finally {
             UserContext.removeUser();
+        }
+    }
+
+    private void returnJson(HttpServletResponse response, String json) throws Exception {
+        PrintWriter writer = null;
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=utf-8");
+        try {
+            writer = response.getWriter();
+            writer.print(json);
+        } catch (IOException e) {
+        } finally {
+            if (writer != null) {
+                writer.close();
+            }
         }
     }
 }
