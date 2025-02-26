90e42d01eeb83fd6b8c5682a248835b5e98e66e4
TwentyfiveBTea <1248102041@qq.com>
2025-02-26 15:02:36 +0800

    提交MySQL数据库分页插件

---
+++ project/src/main/java/com/btea/shortlink/project/config/DataBaseConfiguration.java
@@ -0,0 +1,25 @@
+package com.btea.shortlink.project.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: TwentyFiveBTea
+ * @Date: 2025/2/26 14:12
+ * @Description: 数据库配置
+ */
+@Configuration
+public class DataBaseConfiguration {
+    /**
+     * 分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
