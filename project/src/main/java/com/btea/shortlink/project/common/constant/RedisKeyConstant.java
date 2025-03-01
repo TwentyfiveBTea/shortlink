7187b4017d0ef8b73d34dd7a20e2333bceccb455
TwentyfiveBTea <1248102041@qq.com>
2025-03-01 13:57:51 +0800

    短链接跳转原始链接开发缓存击穿功能

---
+++ project/src/main/java/com/btea/shortlink/project/common/constant/RedisKeyConstant.java
@@ -0,0 +1,19 @@
+package com.btea.shortlink.project.common.constant;
+
+/**
+ * @Author: TwentyFiveBTea
+ * @Date: 2025/2/28 12:39
+ * @Description: Redis Key 常量类
+ */
+public class RedisKeyConstant {
+
+    /**
+     * 短链接跳转前缀 Key
+     */
+    public static final String GOTO_SHORT_LINK_KEY = "short-link_goto_%s";
+
+    /**
+     * 短链接跳转锁前缀 Key
+     */
+    public static final String LOCK_GOTO_SHORT_LINK_KEY = "short-link_lock_goto_%s";
+}
