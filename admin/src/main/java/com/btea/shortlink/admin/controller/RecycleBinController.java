a1d66fdf9b4b873309b1a7da87cda06181a1c261
TwentyfiveBTea <1248102041@qq.com>
2025-03-04 11:05:43 +0800

    开发短链接分页查询回收站功能

--- admin/src/main/java/com/btea/shortlink/admin/controller/RecycleBinController.java
+++ admin/src/main/java/com/btea/shortlink/admin/controller/RecycleBinController.java
@@ -1,10 +1,14 @@
 package com.btea.shortlink.admin.controller;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.btea.shortlink.admin.common.convention.result.Result;
 import com.btea.shortlink.admin.common.convention.result.Results;
 import com.btea.shortlink.admin.dto.req.RecycleBinSaveReqDTO;
 import com.btea.shortlink.admin.remote.ShortLinkRemoteService;
+import com.btea.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
+import com.btea.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,10 +28,14 @@ public class RecycleBinController {
     private final ShortLinkRemoteService shortLinkRemoteService = new ShortLinkRemoteService() {
     };
 
-
     @PostMapping("/api/short-link/v1/recycle-bin/save")
     public Result<Void> saveRecycleBin(@RequestBody RecycleBinSaveReqDTO requestParam) {
         shortLinkRemoteService.saveRecycleBin(requestParam);
         return Results.success();
+    }
+
+    @GetMapping("/api/short-link/admin/v1/recycle-bin/page")
+    public Result<IPage<ShortLinkPageRespDTO>> pageShortLink(ShortLinkPageReqDTO requestParam) {
+        return shortLinkRemoteService.pageRecycleBinShortLink(requestParam);
     }
 }
--- project/src/main/java/com/btea/shortlink/project/controller/RecycleBinController.java
+++ project/src/main/java/com/btea/shortlink/project/controller/RecycleBinController.java
@@ -1,10 +1,14 @@
 package com.btea.shortlink.project.controller;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.btea.shortlink.project.common.convention.result.Result;
 import com.btea.shortlink.project.common.convention.result.Results;
 import com.btea.shortlink.project.dto.req.RecycleBinSaveReqDTO;
+import com.btea.shortlink.project.dto.req.ShortLinkPageReqDTO;
+import com.btea.shortlink.project.dto.resp.ShortLinkPageRespDTO;
 import com.btea.shortlink.project.service.RecycleBinService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,11 +22,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class RecycleBinController {
 
-    private RecycleBinService recycleBinService;
+    private final RecycleBinService recycleBinService;
 
     @PostMapping("/api/short-link/v1/recycle-bin/save")
     public Result<Void> saveRecycleBin(@RequestBody RecycleBinSaveReqDTO requestParam) {
         recycleBinService.saveRecycleBin(requestParam);
         return Results.success();
+    }
+
+    @GetMapping("/api/short-link/v1/recycle-bin/page")
+    public Result<IPage<ShortLinkPageRespDTO>> pageShortLink(ShortLinkPageReqDTO requestParam) {
+        return Results.success(recycleBinService.pageShortLink(requestParam));
     }
 }
