425a02fc6341517df1d07fc14881d15e4957b818
TwentyfiveBTea <1248102041@qq.com>
2025-03-06 12:26:38 +0800

    开发短链接监控之高频访问IP数据

---
+++ project/src/main/java/com/btea/shortlink/project/dao/entity/LinkAccessLogsDO.java
@@ -0,0 +1,56 @@
+package com.btea.shortlink.project.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.btea.shortlink.project.common.database.BaseDO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author: TwentyFiveBTea
+ * @Date: 2025/3/6 11:53
+ * @Description: 访问日志监控实体
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName("t_link_access_logs")
+public class LinkAccessLogsDO extends BaseDO {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 完整短链接
+     */
+    private String fullShortUrl;
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+
+    /**
+     * 用户信息
+     */
+    private String user;
+
+    /**
+     * 浏览器
+     */
+    private String browser;
+
+    /**
+     * 操作系统
+     */
+    private String os;
+
+    /**
+     * ip
+     */
+    private String ip;
+}
---
+++ project/src/main/java/com/btea/shortlink/project/dao/mapper/LinkAccessLogsMapper.java
@@ -0,0 +1,12 @@
+package com.btea.shortlink.project.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.btea.shortlink.project.dao.entity.LinkAccessLogsDO;
+
+/**
+ * @Author: TwentyFiveBTea
+ * @Date: 2025/3/6 11:55
+ * @Description: 访问日志监控持久层
+ */
+public interface LinkAccessLogsMapper extends BaseMapper<LinkAccessLogsDO> {
+}
