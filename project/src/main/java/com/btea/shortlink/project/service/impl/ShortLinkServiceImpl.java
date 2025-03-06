7e19a53bf87c8fc25764b11af16a532e21cd3e58
TwentyfiveBTea <1248102041@qq.com>
2025-03-06 11:30:32 +0800

    开发短链接监控之操作系统访问数据

--- project/src/main/java/com/btea/shortlink/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com/btea/shortlink/project/service/impl/ShortLinkServiceImpl.java
@@ -19,14 +19,8 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.btea.shortlink.project.common.convention.exception.ClientException;
 import com.btea.shortlink.project.common.convention.exception.ServiceException;
 import com.btea.shortlink.project.common.enums.ValidDateTypeEnum;
-import com.btea.shortlink.project.dao.entity.LinkAccessStatsDO;
-import com.btea.shortlink.project.dao.entity.LinkLocaleStatsDO;
-import com.btea.shortlink.project.dao.entity.ShortLinkDO;
-import com.btea.shortlink.project.dao.entity.ShortLinkGotoDO;
-import com.btea.shortlink.project.dao.mapper.LinkAccessStatsMapper;
-import com.btea.shortlink.project.dao.mapper.LinkLocaleStatsMapper;
-import com.btea.shortlink.project.dao.mapper.ShortLinkGotoMapper;
-import com.btea.shortlink.project.dao.mapper.ShortLinkMapper;
+import com.btea.shortlink.project.dao.entity.*;
+import com.btea.shortlink.project.dao.mapper.*;
 import com.btea.shortlink.project.dto.req.ShortLinkCreateReqDTO;
 import com.btea.shortlink.project.dto.req.ShortLinkPageReqDTO;
 import com.btea.shortlink.project.dto.req.ShortLinkUpdateRepDTO;
@@ -81,6 +75,7 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
     private final ShortLinkGotoMapper shortLinkGotoMapper;
     private final LinkAccessStatsMapper linkAccessStatsMapper;
     private final LinkLocaleStatsMapper linkLocaleStatsMapper;
+    private final LinkOsStatsMapper linkOsStatsMapper;
 
     @Value("${short-link.stats.locale.amap-key}")
     private String statsLocaleAmapKey;
@@ -334,6 +329,14 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
                         .date(new Date())
                         .build();
                 linkLocaleStatsMapper.shortLinkLocaleStats(linkLocaleStatsDO);
+                LinkOsStatsDO linkOsStatsDO = LinkOsStatsDO.builder()
+                        .os(LinkUtil.getOs((HttpServletRequest) request))
+                        .cnt(1)
+                        .gid(gid)
+                        .fullShortUrl(fullShortUrl)
+                        .date(new Date())
+                        .build();
+                linkOsStatsMapper.shortLinkOsState(linkOsStatsDO);
             }
         } catch (Throwable ex) {
             log.error("短链接访问量统计异常", ex);
