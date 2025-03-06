742559176735d9b90ab8eade4a90a67eb362c519
TwentyfiveBTea <1248102041@qq.com>
2025-03-06 11:41:37 +0800

    开发短链接监控之浏览器访问数据

---
+++ project/src/main/java/com/btea/shortlink/project/dao/entity/LinkBrowserStatsDO.java
@@ -0,0 +1,52 @@
+package com.btea.shortlink.project.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * @Author: TwentyFiveBTea
+ * @Date: 2025/3/6 11:32
+ * @Description: 浏览器统计访问实体
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName("t_link_browser_stats")
+public class LinkBrowserStatsDO {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 完整短链接
+     */
+    private String fullShortUrl;
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+
+    /**
+     * 日期
+     */
+    private Date date;
+
+    /**
+     * 访问量
+     */
+    private Integer cnt;
+
+    /**
+     * 浏览器
+     */
+    private String browser;
+}
