0d71d5dcade1a0c0d63e342a67f58be58271abdd
TwentyfiveBTea <1248102041@qq.com>
2025-04-15 21:37:17 +0800

    添加注解

--- project/src/main/java/com/btea/shortlink/project/dto/resp/ShortLinkStatsAccessDailyRespDTO.java
+++ project/src/main/java/com/btea/shortlink/project/dto/resp/ShortLinkStatsAccessDailyRespDTO.java
@@ -1,6 +1,7 @@
 package com.btea.shortlink.project.dto.resp;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.Date;
@@ -11,6 +12,7 @@ import java.util.Date;
  * @Description: 短链接基础访问监控响应参数
  */
 @Data
+@Builder
 public class ShortLinkStatsAccessDailyRespDTO {
 
     /**
