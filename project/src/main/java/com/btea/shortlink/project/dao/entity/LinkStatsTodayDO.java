5fa026f4283e086fd48c97630d3b40060e61d820
TwentyfiveBTea <1248102041@qq.com>
2025-04-15 17:11:59 +0800

    开发短链接监控之今日统计

--- project/src/main/java/com/btea/shortlink/project/dao/entity/LinkStatsTodayDO.java
+++ project/src/main/java/com/btea/shortlink/project/dao/entity/LinkStatsTodayDO.java
@@ -2,7 +2,10 @@ package com.btea.shortlink.project.dao.entity;
 
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.btea.shortlink.project.common.database.BaseDO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
@@ -13,6 +16,9 @@ import java.util.Date;
  */
 @TableName("t_link_stats_today")
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LinkStatsTodayDO extends BaseDO {
 
     /**
--- project/src/main/java/com/btea/shortlink/project/dao/mapper/LinkStatsTodayMapper.java
+++ project/src/main/java/com/btea/shortlink/project/dao/mapper/LinkStatsTodayMapper.java
@@ -2,6 +2,8 @@ package com.btea.shortlink.project.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.btea.shortlink.project.dao.entity.LinkStatsTodayDO;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * @Author: TwentyFiveBTea
@@ -9,4 +11,13 @@ import com.btea.shortlink.project.dao.entity.LinkStatsTodayDO;
  * @Description: 短链接今日统计持久层
  */
 public interface LinkStatsTodayMapper extends BaseMapper<LinkStatsTodayDO> {
+
+    /**
+     * 记录今日统计监控数据
+     */
+    @Insert("INSERT INTO " +
+            "t_link_stats_today (full_short_url, gid, date, today_uv, today_pv, today_uip, create_time, update_time, del_flag) " +
+            "VALUES( #{linkTodayStats.fullShortUrl}, #{linkTodayStats.gid}, #{linkTodayStats.date}, #{linkTodayStats.todayPv}, #{linkTodayStats.todayUv}, #{linkTodayStats.todayUip}, NOW(), NOW(), 0) " +
+            "ON DUPLICATE KEY UPDATE today_uv = today_uv +  #{linkTodayStats.todayUv}, today_pv = today_pv +  #{linkTodayStats.todayPv}, today_uip = today_uip +  #{linkTodayStats.todayUip};")
+    void shortLinkTodayState(@Param("linkTodayStats") LinkStatsTodayDO linkStatsTodayDO);
 }
