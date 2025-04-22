589956fda2d30bfa8afd0e1e52d4e53966f75429
TwentyfiveBTea <1248102041@qq.com>
2025-04-22 18:26:54 +0800

    短链接后管通过风控保障系统稳定性

---
+++ admin/src/main/java/com/btea/shortlink/admin/config/UserConfiguration.java
@@ -0,0 +1,46 @@
+package com.btea.shortlink.admin.config;
+
+import com.btea.shortlink.admin.common.biz.user.UserFlowRiskControlFilter;
+import com.btea.shortlink.admin.common.biz.user.UserTransmitFilter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+/**
+ * @Author: TwentyFiveBTea
+ * @Date: 2025/2/24 17:34
+ * @Description: 用户配置自动装配
+ */
+@Configuration
+public class UserConfiguration {
+
+    /**
+     * 用户信息传递过滤器
+     */
+    @Bean
+    public FilterRegistrationBean<UserTransmitFilter> globalUserTransmitFilter(StringRedisTemplate stringRedisTemplate) {
+        FilterRegistrationBean<UserTransmitFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new UserTransmitFilter(stringRedisTemplate));
+        registration.addUrlPatterns("/*");
+        registration.addInitParameter("Login", "/api/short-link/admin/v1/user/login");
+        registration.setOrder(0);
+        return registration;
+    }
+
+    /**
+     * 用户操作流量风控过滤器
+     */
+    @Bean
+    @ConditionalOnProperty(name = "short-link.flow-limit.enable", havingValue = "true")
+    public FilterRegistrationBean<UserFlowRiskControlFilter> globalUserFlowRiskControlFilter(
+            StringRedisTemplate stringRedisTemplate,
+            UserFlowRiskControlConfiguration userFlowRiskControlConfiguration) {
+        FilterRegistrationBean<UserFlowRiskControlFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new UserFlowRiskControlFilter(stringRedisTemplate, userFlowRiskControlConfiguration));
+        registration.addUrlPatterns("/*");
+        registration.setOrder(10);
+        return registration;
+    }
+}
