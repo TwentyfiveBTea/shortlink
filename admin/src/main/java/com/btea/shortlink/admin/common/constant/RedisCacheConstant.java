194d9b62c76d362cf87c7c965c7e74184750e70b
TwentyfiveBTea <1248102041@qq.com>
2025-04-22 17:47:51 +0800

    优化单个用户创建番组数量不能大于20

--- admin/src/main/java/com/btea/shortlink/admin/common/constant/RedisCacheConstant.java
+++ admin/src/main/java/com/btea/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -6,5 +6,14 @@ package com.btea.shortlink.admin.common.constant;
  * @Description: 短链接后管 Reids 缓存常量类
  */
 public class RedisCacheConstant {
+
+    /**
+     * 用户注册分布式锁
+     */
     public static final String LOCK_USER_REGISTER_KEY = "short-link:lock_user-register:";
+
+    /**
+     * 分组创建分布式锁
+     */
+    public static final String LOCK_GROUP_CREATE_KEY = "short-link:lock_group-create:%s";
 }
