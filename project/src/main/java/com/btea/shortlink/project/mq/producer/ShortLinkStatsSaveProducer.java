a332b1ebd6cd67f31f21fa5a13a08e62f8054797
TwentyfiveBTea <1248102041@qq.com>
2025-04-25 10:15:29 +0800

    保持代码规范将缓存 Key 移至常量类

--- project/src/main/java/com/btea/shortlink/project/mq/producer/ShortLinkStatsSaveProducer.java
+++ project/src/main/java/com/btea/shortlink/project/mq/producer/ShortLinkStatsSaveProducer.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
+import static com.btea.shortlink.project.common.constant.RedisKeyConstant.SHORT_LINK_STATS_STREAM_TOPIC_KEY;
+
 /**
  * @Author: TwentyFiveBTea
  * @Date: 2025/4/23 11:11
@@ -18,13 +20,10 @@ public class ShortLinkStatsSaveProducer {
 
     private final StringRedisTemplate stringRedisTemplate;
 
-    @Value("${spring.data.redis.channel-topic.short-link-stats}")
-    private String topic;
-
     /**
      * 发送延迟消费短链接统计
      */
     public void send(Map<String, String> producerMap) {
-        stringRedisTemplate.opsForStream().add(topic, producerMap);
+        stringRedisTemplate.opsForStream().add(SHORT_LINK_STATS_STREAM_TOPIC_KEY, producerMap);
     }
 }
