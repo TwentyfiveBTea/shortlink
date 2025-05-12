7fba24c76e532443dbbdd9691cc6deeaf8cd316e
TwentyfiveBTea <1248102041@qq.com>
2025-05-12 14:33:13 +0800

    开发短链接系统聚合服务

--- admin/src/main/java/com/btea/shortlink/admin/config/DataBaseConfiguration.java
+++ admin/src/main/java/com/btea/shortlink/admin/config/DataBaseConfiguration.java
@@ -3,6 +3,7 @@ package com.btea.shortlink.admin.config;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
