9dbee8dbd81bf5662a87d6da86b5797442b18fbb
TwentyfiveBTea <1248102041@qq.com>
2025-05-12 20:41:25 +0800

    修复消息队列运行中读取数据为空停止异常

--- project/src/main/java/com/btea/shortlink/project/mq/consumer/ShortLinkStatsSaveConsumer.java
+++ project/src/main/java/com/btea/shortlink/project/mq/consumer/ShortLinkStatsSaveConsumer.java
@@ -79,6 +79,7 @@ public class ShortLinkStatsSaveConsumer implements StreamListener<String, MapRec
         } catch (Throwable ex) {
             messageQueueIdempotentHandler.delMessageProcessed(id.toString());
             log.error("记录短链接监控消费异常", ex);
+            throw ex;
         }
         messageQueueIdempotentHandler.setAccomplish(id.toString());
     }
