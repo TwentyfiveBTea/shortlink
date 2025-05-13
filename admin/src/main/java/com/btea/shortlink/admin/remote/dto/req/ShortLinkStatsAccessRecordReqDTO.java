f5b352ff2bd670f04209724098d5f139a50c3fc9
TwentyfiveBTea <1248102041@qq.com>
2025-05-13 13:57:03 +0800

    修改bug

---
+++ admin/src/main/java/com/btea/shortlink/admin/remote/dto/req/ShortLinkStatsAccessRecordReqDTO.java
@@ -0,0 +1,38 @@
+package com.btea.shortlink.admin.remote.dto.req;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import lombok.Data;
+
+/**
+ * @Author: TwentyFiveBTea
+ * @Date: 2025/5/13 13:54
+ * @Description: 短链接监控访问记录请求参数
+ */
+@Data
+public class ShortLinkStatsAccessRecordReqDTO extends Page {
+
+    /**
+     * 完整短链接
+     */
+    private String fullShortUrl;
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+
+    /**
+     * 开始日期
+     */
+    private String startDate;
+
+    /**
+     * 结束日期
+     */
+    private String endDate;
+
+    /**
+     * 启用标识 0：启用 1：未启用
+     */
+    private Integer enableStatus;
+}
